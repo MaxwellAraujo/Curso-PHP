08beb57c77cba28ab566ce25bf67c785b784a13b
MaxwellAraujo <maxwellaraujo.will@gmail.com>
2021-05-02 23:57:36 -0300

    Ajuste de sinal do codigo

--- Aula09/exercicio02.php
+++ Aula09/exercicio02.php
@@ -13,7 +13,7 @@
         $a = isset( $_GET ["ano"])?$_GET ["ano"] :1900;
         $i = date("Y") - $a;
         echo "Você nasceu em $a e tem $i anos.<br>";
-        if ($i > 16){
+        if ($i < 16){
             $tipovoto = "não vota";
         } else if (($i >= 16 && $i < 18) || ($i > 65)) {
             $tipovoto = "voto opcional";
